dd73dc37a1682f8f3d5aa120a4271d3055fa49b0
ypskalyan <165227072+ypskalyan@users.noreply.github.com>
2026-04-21 19:04:47 -0500

    Configure Rollup output entry file names
    Add custom entry file naming for build output

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/prep-dashboard/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash]-v2.js`,
+      }
+    }
+  }
 })
